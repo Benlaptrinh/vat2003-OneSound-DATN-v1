2b0064179bb560c8bbf393cc213ca91586609e3a
Pongsua <baokha2923@gmail.com>
2024-03-13 13:50:45 +0700

    siêu cấp ultra favorite youtube song và có 1 tí album nữa nhớ gọp cho đủ

--- src/main/java/com/project/shopapp/repository/FavoriteAlbumDAO.java
+++ src/main/java/com/project/shopapp/repository/FavoriteAlbumDAO.java
@@ -1,10 +1,12 @@
 package com.project.shopapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.shopapp.composite.FavoriteAlbumId;
 import com.project.shopapp.entity.FavoriteAlbum;
 
 public interface FavoriteAlbumDAO extends JpaRepository<FavoriteAlbum, FavoriteAlbumId> {
-
+    List<FavoriteAlbum> findByUserId(Long userId);
 }
