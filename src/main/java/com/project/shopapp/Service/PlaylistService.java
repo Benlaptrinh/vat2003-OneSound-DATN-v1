1c731702276f9cf85fc771bec23212f51b96f526
Benlaptrinh <nguyenhuuviet159@gmail.com>
2024-03-13 21:01:39 +0700

    a

--- src/main/java/com/project/shopapp/Service/PlaylistService.java
+++ src/main/java/com/project/shopapp/Service/PlaylistService.java
@@ -19,6 +19,8 @@ public interface PlaylistService {
 
     Optional<Playlist> findByName(String name);
 
+    Optional<Playlist> findById(Long id);
+
     List<Playlist> findByUser_id(Long userId);
 
 }
